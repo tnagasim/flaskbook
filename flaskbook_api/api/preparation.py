c47e1686bb5b2eee7f3758b2da0478f0867ab71a
tnagasim <40717403+tnagasim@users.noreply.github.com>
2025-04-25 14:18:57 +0000

    fix: mypyエラー修正. PIL.Imageのimportを変更

--- flaskbook_api/api/preparation.py
+++ flaskbook_api/api/preparation.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-import PIL
+from PIL import Image
 
 basedir = Path(__file__).parent.parent
 
@@ -9,6 +9,6 @@ def load_image(request, reshaped_size=(256, 256)):
     """画像の読み込み"""
     filename = request.json["filename"]
     dir_image = str(basedir / "data" / "original" / filename)
-    image_obj = PIL.Image.open(dir_image).convert("RGB")
+    image_obj = Image.open(dir_image).convert("RGB")
     image = image_obj.resize(reshaped_size)
     return image, filename
